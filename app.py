ef2a316ce070a36ef7811c7e10565ea88e85c471
feitosalavinia <124395934+feitosalavinia@users.noreply.github.com>
2024-05-23 09:34:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
 @app.route('/')
-def devops():
- return '<center><h1><font color=red>Germinare Tech, EU AMO DEVOPS</center>'
+  def devops():
+  return '<center><h1><font color=red>Germinare Tech, EU AMO DEVOPS</center>'
+  if __name__ == '__main__':
+  app.run(debug=True, host='0.0.0.0')
+ 
+@app.route('/salah')
+  def salah():
+  return '<center><h1><font color=red>Germinare Tech, EU AMO DEVOPS2</center>'
 if __name__ == '__main__':
- app.run(debug=True, host='0.0.0.0')
+app.run(debug=True, host='0.0.0.0')
